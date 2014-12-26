c381ede4e2162e8219d758ef0d5d9bd5534d0cf5
Lucius Choi <rorlab@gmail.com>
2014-12-26 12:17:35 +0900

    change ruby vertion to 2.2.0

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,15 +6,18 @@ function install {
     apt-get -y install "$@" >/dev/null 2>&1
 }
 
+# fixing the warning message - "stdin: is not a tty"
+sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile
+
 echo updating package information
 apt-add-repository -y ppa:brightbox/ruby-ng >/dev/null 2>&1
 apt-get -y update >/dev/null 2>&1
 
 install 'development tools' build-essential
 
-install Ruby ruby2.1 ruby2.1-dev
-update-alternatives --set ruby /usr/bin/ruby2.1 >/dev/null 2>&1
-update-alternatives --set gem /usr/bin/gem2.1 >/dev/null 2>&1
+install Ruby ruby2.2 ruby2.2-dev
+update-alternatives --set ruby /usr/bin/ruby2.2 >/dev/null 2>&1
+update-alternatives --set gem /usr/bin/gem2.2 >/dev/null 2>&1
 
 echo installing Bundler
 gem install bundler -N >/dev/null 2>&1
